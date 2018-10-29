fe508894a8bc88e3db87002459cac800d5bd4b15
Mattia Fogli <mattiafogli9@gmail.com>
2018-10-29 10:15:32 +0100

    Configurazione Guardfile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'user/new'
   get 'user/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  root 'pages#index'
 
   resources :users, only: [:new, :create]
 
