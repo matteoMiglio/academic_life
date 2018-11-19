2a54a98e636ff73a6b7a571153ca8560a5278f8f
Mattia Fogli <mattiafogli9@gmail.com>
2018-11-19 10:07:08 +0100

    Aggiunta test model del corso

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,10 @@
 class Course < ApplicationRecord
+  validates :name, presence: true,
+                   length: { maximum: 100 }
+  validates :credit, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :professor, presence: true,
+                        length: { maximum: 100 }
+
   has_one :message_board
   has_and_belongs_to_many :users
   has_and_belongs_to_many :degrees
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @course = Course.new(name: "Analisi 1", credit: 6, professor: "Nathan")
+  end
+
+  test "should be valid" do
+    assert @course.valid?
+  end
+
+  test "name should be present" do
+    @course.name = ""
+    assert_not @course.valid?
+  end
+ 
+  test "name should not be too long" do
+    @course.name = "a" * 101
+    assert_not @course.valid?
+  end
+
+  test "credit should be present" do
+    @course.credit = nil
+    assert_not @course.valid?
+  end
+
+  test "credit should be >= 0" do
+    @course.credit = -4
+    assert_not @course.valid?
+  end
+
+  test "credit should be integer" do
+    @course.credit = 1.5
+    assert_not @course.valid?
+  end
+
+  test "professor should be present" do
+    @course.professor = ""
+    assert_not @course.valid?
+  end
+
+  test "professor should not be too long" do
+    @course.professor = "a" * 101
+    assert_not @course.valid?
+  end
 end
