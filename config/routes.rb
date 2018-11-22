ea98d84adfddcf8c759b9bca26107d07d16cf2f8
Mattia Fogli <mattiafogli9@gmail.com>
2018-11-22 10:51:32 +0100

    Visualizzazione primitiva del singolo post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'user_sessions#new'
 
