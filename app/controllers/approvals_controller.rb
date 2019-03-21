166b75e1b0e98d09cffb13282951d5d829f2abde
Andrea <fortinia95@gmail.com>
2019-03-21 20:39:34 +0100

    Miglioramenti secondari. Riordino codice

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -2,8 +2,9 @@ class ApprovalsController < ApplicationController
   before_action :find_post
 
   def create
-    if not already_approval?
-      if @post.approvals.create(user: current_user)
+    if not already_approved?
+      @approval = @post.approvals.create(user: current_user)
+      if @approval
         respond_to do |format|
           format.js
           format.html { redirect_to post_path(@post) }
@@ -13,7 +14,7 @@ class ApprovalsController < ApplicationController
   end
 
   def destroy
-    if already_approval?
+    if already_approved?
       @post.approvals.where(user: current_user).destroy_all
       respond_to do |format|
         format.js
@@ -28,7 +29,7 @@ class ApprovalsController < ApplicationController
     @post = Post.find(params[:post_id])
   end
 
-  def already_approval?
+  def already_approved?
     Approval.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
