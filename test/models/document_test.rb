2d9fe5629f6855d17637d7ea0c5476e54512c86d
Matteo Miglioli <matteo.miglioli95@gmail.com>
2019-04-23 16:03:36 +0200

    Implementato test per il model document.

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:luca)
+    @message_board = message_boards(:rdc)
+    @category = categories(:appunti)
+    @document = Document.new(name: "Doc", description: "Prova di documento", 
+                             user_id: @user.id, message_board_id: @message_board.id,
+                             category_id: @category.id)
+  end
+
+  test "should be valid" do
+    assert @document.valid?
+  end
+
+  test "name should be present" do
+    @document.name = nil
+    assert_not @document.valid?
+  end
+
+  test "name should be at most 100 characters" do
+    @document.name = "a" * 101
+    assert_not @document.valid?
+  end
+
+  test "description should be present" do
+    @document.description = nil
+    assert_not @document.valid?
+  end
+
+  test "description should be at most 300 characters" do
+    @document.description = "a" * 301
+    assert_not @document.valid?
+  end
+
+  test "user id should be present" do
+    @document.user_id = nil
+    assert_not @document.valid?
+  end
+
+  test "message board id should be present" do
+    @document.message_board_id = nil
+    assert_not @document.valid?
+  end
+
+  test "category id should be present" do
+    @document.category_id = nil
+    assert_not @document.valid?
+  end
 end
