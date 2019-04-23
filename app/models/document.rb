3957b9db5c9156fca8aa8d98e8b9158c8be750cb
Matteo Miglioli <matteo.miglioli95@gmail.com>
2019-04-23 15:53:56 +0200

    Implementati test relativi al documents_controller.

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,10 +3,12 @@ class Document < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  has_one_attached :document
+
   validates :message_board, presence: true
   validates :user, presence: true
   validates :category, presence: true
   validates :name, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 300 }
-  validates :path, presence: true, length: { maximum: 70 }
+  #validates :path, presence: true, length: { maximum: 70 }
 end
