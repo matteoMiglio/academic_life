8b697eded3cc9321a6ae329bb9838b1533d7eafa
Mattia Fogli <mattiafogli9@gmail.com>
2019-05-01 12:49:54 +0200

    Refactoring partial post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+  def find_my_approval(approvals)
+    approvals.find { |approval| approval.user_id == current_user.id }
+  end
+
+  def show_link(post)
+    options = { class: "card-link" }
+    url = message_board_post_url(post.message_board_id, post.id)
+    link_to "Show", url, options if current_page?(action: 'index')
+  end
+
+  def delete_link(post)
+    options = { method: :delete,
+                data: { confirm: "Sei sicuro?" },
+                class: "card-link" }
+    url = message_board_post_url(post.message_board_id, post.id)
+    link_to "Delete", url, options if current_user.id == post.user_id
+  end
 end
