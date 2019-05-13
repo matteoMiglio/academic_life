3ff016419de95b60979a39468346ea11c682d5cf
Andrea <fortinia95@gmail.com>
2019-05-13 10:36:10 +0200

    Correzione test controller per gli eventi

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  setup do 
     @user = users(:luca)
     @message_board = message_boards(:rdc)
     @group = groups(:lafayette)
     @event = events(:evento)
   end
 
-  test "should get show" do 
-    login(@user)
-    get message_board_group_event_url(@message_board, @group, @event)
-    assert_response :success
-  end
-
   test "should get create" do
     login(@user)
     assert_difference "Event.count" do
-      post message_board_group_events_url(@message_board, @group), params: { event: { name: "Nome Evento",
-                                                                                      description: "Descrizione di prova",
-                                                                                      appointment: "2020-05-05 16:06:45",
-                                                                                      place: "Luogo dell'evento",
-                                                                                      group_id: @group.id } }
+      post message_board_group_events_url(@message_board, @group), params: { event: { name: @event.name,
+                                                                        description: @event.description,
+                                                                        appointment: @event.appointment,
+                                                                        place: @event.place,
+                                                                        group_id: @group.id } }
       end
     assert_redirected_to message_board_group_url(@message_board, @group)
+  end
+
+  test "should get new" do
+    login(@user)
+    get new_message_board_group_event_url(@message_board, @group)
+    assert_response :success
   end
 
   test "should get destroy" do
