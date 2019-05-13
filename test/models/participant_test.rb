f6e81d990d0ffea79b6e2a5af68ff6799c624abb
Andrea <fortinia95@gmail.com>
2019-05-13 09:48:48 +0200

    Aggiunta test model per la tabella 'participants'

--- test/models/participant_test.rb
+++ test/models/participant_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ParticipantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:luca)
+    @participant = participants(:luca)
+    @member = participants(:giovanni)
+    @creator = participants(:luca)
+  end
+
+  test "should be valid" do
+    assert @participant.valid?
+  end
+
+  test "user_id should be present" do
+    @participant.user_id = nil
+    assert_not @participant.valid?
+  end
+
+  test "event_id should be present" do
+    @participant.event_id = nil
+    assert_not @participant.valid?
+  end
+
+  test "role should be present" do
+    @participant.role = nil
+    assert_not @participant.valid?
+  end
+
+  test "role must be 'creator' or 'member'" do
+    @participant.role = 'a'*6
+    assert_not @participant.valid?
+  end
 end
